66d8aae5730c9c51ed7c49f01309e27d723199bf
scalonmc <69158137+scalonmc@users.noreply.github.com>
2020-08-07 11:14:12 -0300

    Update initLesson.R

--- Aula1/initLesson.R
+++ Aula1/initLesson.R
@@ -1,8 +1,2 @@
-.get_course_path <- function(){
-  tryCatch(swirl:::swirl_courses_dir(),
-           error = function(c) {file.path(find.package("swirl"),"Courses")}
-  )
-}
-
 # Read in data
-dados <- read.csv(file.path(.get_course_path(), "Aula1", "dados.csv"))
+dados <- read.csv("https://github.com/scalonmc/Aula1/blob/master/Aula1/dados.csv")
