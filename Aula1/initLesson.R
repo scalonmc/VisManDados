370165a51bbe122b4d906926599a9cb5fd2d4d77
scalonmc <69158137+scalonmc@users.noreply.github.com>
2021-07-26 11:11:28 -0300

    Update initLesson.R

--- Aula1/initLesson.R
+++ Aula1/initLesson.R
@@ -1,2 +1,3 @@
 require(rjson)
-dados <- fromJSON("https://raw.githubusercontent.com/scalonmc/Aula1/master/Aula1/dados.csv")
+url = "https://raw.githubusercontent.com/scalonmc/Aula1/master/Aula1/dados.csv"
+dados <- fromJSON((url))
