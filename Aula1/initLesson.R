87c41ffa10c78e2aea91282026310c2c9357c7ad
scalonmc <69158137+scalonmc@users.noreply.github.com>
2021-07-26 11:24:01 -0300

    Update initLesson.R

--- Aula1/initLesson.R
+++ Aula1/initLesson.R
@@ -1,3 +1,3 @@
 require(RCurl)
-x = getURL("https://raw.github.com/scalonmc/Aula1/master/Aula1/dados.csv")
+x = getURL("https://raw.github.com/scalonmc/VisManDados/blob/ccfaa9aeb96fc7151348ef3a3e4423c917dde521/Aula1/dados.csv")
 dados <- read.csv(text = x)
